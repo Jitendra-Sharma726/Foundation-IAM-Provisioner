e6980aa5f78a7a6961e9d8dc54cf185526dfa1b2
Jitendra Kumar Sharma <136931188+Jitendra-Sharma726@users.noreply.github.com>
2026-06-14 13:07:31 +0530

    Configure AWS provider for local emulator
    Added local AWS emulator configuration to provider.

--- provider.tf
+++ provider.tf
@@ -1,5 +1,16 @@
 provider "aws" {
   region = "us-east-1"
 
-  # other provided configuration remains unchanged
+  # other provided configuration remains 
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    iam = "http://localhost:4566" # Points to your local emulator
+  }
 }
+
+
